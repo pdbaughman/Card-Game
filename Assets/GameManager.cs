650085c2b56945fdff9780c3a64f365b4ed69d8b
Cactomelon <103218222+Cactomelon@users.noreply.github.com>
2025-12-06 01:13:02 -0600

    ui customization
    this is the greatest game ever

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,34 +7,29 @@ public class GameManager : MonoBehaviour
     public GameObject winScreen;  
     public GameObject gameUI;    
 
-    // Called when Start button is pressed
     public void StartGame()
     {
         mainMenu.SetActive(false);
         gameUI.SetActive(true);
     }
 
-    // Called when Exit button is pressed
     public void ExitGame()
     {
         Application.Quit();
         Debug.Log("Game quit! (Does not close in editor)");
     }
 
-    // Called when Reset button is pressed
     public void ResetGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-    // Call this when the player wins
     public void ShowWinScreen()
     {
         winScreen.SetActive(true);
         gameUI.SetActive(false);
     }
 	
-	// Called when Main Menu button pressed
 	public void BackToMainMenu()
 	{
     winScreen.SetActive(false);
